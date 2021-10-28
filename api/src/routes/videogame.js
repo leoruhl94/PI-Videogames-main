239482fbcfe490af786461948eb583c5bbe816b2
leoruhl94 <leoruhl94@gmail.com>
2021-10-28 11:14:42 -0300

    .

--- api/src/routes/videogame.js
+++ api/src/routes/videogame.js
@@ -1,6 +1,8 @@
 const { Router } = require('express');
 const router = Router();
-const { Videogames } = require('../db')
+const { Videogames } = require('../db');
+const { API_KEY } = process.env;
+const axios = require('axios');
 
 
 //Recibe los datos recolectados desde el formulario controlado de la ruta de creación de videojuego por body
@@ -28,9 +30,31 @@ router.post('/', async (req, res, next) => {
 //Obtener el detalle de un videojuego en particular
 //Debe traer solo los datos pedidos en la ruta de detalle de videojuego
 //Incluir los géneros asociados
-router.get('/:id', (req, res, next) => {
+router.get('/:id', async (req, res, next) => {
     const { id } = req.params;
-    res.json({msj: 'entre a: /videogame/:id'})
+    if(typeof id === 'string' && id.length > 8 ){
+        let videogame;
+        //busco en db
+    } else { //busco en api
+        try {
+            let search = await axios.get(`https://api.rawg.io/api/games/${id}?key=${API_KEY}`)
+            //console.log(search, '___________=___')
+            const { name, description, released, rating, platforms, background_image, genres } = search.data;
+            console.log(name, description, released, rating, platforms, '___________=___')
+            res.json({
+                name, 
+                description, 
+                released, 
+                rating,
+                image: background_image,
+                platforms: platforms.map(item => item.platform.name),
+                genres: genres.map(item => item.name)
+            })
+        } catch (error) {
+            next(error);
+        }
+    }
+     
 });    
 
 
