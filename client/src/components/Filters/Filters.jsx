44e421283878c4d80e792c454618c22cdf43cf05
leoruhl94 <leoruhl94@gmail.com>
2021-11-07 17:23:48 -0300

    algunos estilos en home

--- client/src/components/Filters/Filters.jsx
+++ client/src/components/Filters/Filters.jsx
@@ -1,29 +1,35 @@
-import './Filters.css';
+import "./Filters.css";
 import { useDispatch } from "react-redux";
 import { getVideogames } from "../../redux/actions";
 import { WidgetOrderByName } from "../WidgetOrderByName/WidgetOrderByName";
-import { WidgetOrderByRating } from '../WidgetOrderByRating/WidgetOrderByRating';
-import { WidgetFilterFrom } from '../WidgetFilterFrom/WidgetFilterFrom';
-import { SelectedFilter } from '../SelectedFilters/SelectedFilters';
-import { WidgetFilterGenres } from '../WidgetFilterGenres/WidgetFilterGenres';
+import { WidgetOrderByRating } from "../WidgetOrderByRating/WidgetOrderByRating";
+import { WidgetFilterFrom } from "../WidgetFilterFrom/WidgetFilterFrom";
+import { SelectedFilter } from "../SelectedFilters/SelectedFilters";
+import { WidgetFilterGenres } from "../WidgetFilterGenres/WidgetFilterGenres";
+import Icon from "../../assets/Icon/Icon";
 
 export const Filters = () => {
   let dispatch = useDispatch();
 
   const handleOnClick = (e) => {
     let target = e.target.value;
-    console.log(target)
-    if(target==='reset') dispatch(getVideogames())
-  }
+    console.log(target);
+    if (target === "reset") dispatch(getVideogames());
+  };
 
   return (
     <aside className="filters">
-      <SelectedFilter/>
-      <WidgetOrderByName/>
-      <WidgetOrderByRating/>
-      <WidgetFilterFrom/>
-      <WidgetFilterGenres/>
-      <button type='button' onClick={handleOnClick} value='reset'>reset</button>
+      <SelectedFilter />
+      <WidgetOrderByName />
+    
+      <WidgetOrderByRating />
+      <WidgetFilterFrom />
+      <WidgetFilterGenres />
+      <button className="filters_reset" type="button" onClick={handleOnClick} value="reset">
+      <span className="filters_reset_icon">
+        <Icon svg="refresh" title="refresh" />
+      </span>
+      </button>
     </aside>
   );
 };
--- client/src/containers/Home/Home.jsx
+++ client/src/containers/Home/Home.jsx
@@ -1,28 +1,29 @@
 import { useEffect } from "react";
-import { useDispatch } from 'react-redux';
-import {  getGenres, getVideogames } from '../../redux/actions'
-import { Filters } from '../../components/Filters/Filters';
+import { useDispatch } from "react-redux";
+import { getGenres, getVideogames } from "../../redux/actions";
+import { Filters } from "../../components/Filters/Filters";
 import { Pagination } from "../../components/Pagination/Pagination";
-import './Home.css'
+import "./Home.css";
 import { Cards } from "../../components/Cards/Cards";
 import { SearchBar } from "../../components/SearchBar/SearchBar";
 
-
-export const Home = () =>{
-
-    let dispatch = useDispatch();
-    useEffect( () => {
-        dispatch(getGenres())
-        dispatch(getVideogames())
-    }, [dispatch])
-    return (
-        <section className="home">
-            <SearchBar/>
-            <div className="home_games grid_container"> 
-                <Filters/>
-                <Cards/>
-            </div>
-            <Pagination/>
-        </section>
-    )
-}
+export const Home = () => {
+  let dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getGenres());
+    dispatch(getVideogames());
+  }, [dispatch]);
+  return (
+    <div className="home_background">
+      <section className="home">
+        <SearchBar />
+        <Filters />
+        <div className="home_games">
+          {/* <Filters/> */}
+          <Cards />
+        </div>
+        <Pagination />
+      </section>
+    </div>
+  );
+};
